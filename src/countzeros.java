fecfc7fc3408e97f5c547429c6fedcf3dbf8f8c5
gourimundada3-stack <gourimundada3@gmail.com>
2026-09-22 19:56:02 +0530

    solved using recursion

---
+++ src/countzeros.java
@@ -0,0 +1,18 @@
+public class countzeros {
+    public static void main(String[] args) {
+        System.out.println(count(30004));
+    }
+    static int count(int n){
+        return countZeroes(n,0);
+    }
+    private  static int countZeroes(int n,int c){
+        if(n==0){
+            return c;
+        }
+        int rem = n%10 ;
+        if(rem == 0){
+            return countZeroes(n/10,c+1);
+        }
+        return countZeroes(n/10 , c);
+    }
+}
